f4f074325d8c8d2072f644f1024e7f5d40f1a1be
wachirawit1 <69790127+wachirawit1@users.noreply.github.com>
2020-09-14 17:06:13 +0700

    เรียบ

---
+++ Lab7DialogRV/app/src/main/java/com/example/lab7dialogrv/StudentsAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.lab7dialogrv
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.lab7dialogrv.R
+import com.example.lab7dialogrv.Student
+
+class StudentsAdapter(val item : List<Student>, val context : Context): RecyclerView.Adapter <ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view_item = LayoutInflater.from(parent.context).inflate(R.layout.std_item_layout,parent, false)
+        return  ViewHolder(view_item)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.tvID.text = item[position].id
+        holder.tvName.text = item[position].name
+        holder.tvAge.text = item[position].age.toString()
+    }
+
+    override fun getItemCount(): Int {
+        return item.size
+    }
+}
